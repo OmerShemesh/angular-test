1fb09e44bf0d6045bb4e15121c0ec014e743314a
Omer Shemsheyan <omershemesh95@gmail.com>
2016-06-30 15:19:54 +0300

    fixed path

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {CustomerCompnent} from './/customer/customer.component';
+import {CustomerCompnent} from './customer/customer.component';
 @Component({
     selector: 'my-app',
     templateUrl:'app/app.component.html',
